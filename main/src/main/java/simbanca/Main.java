54359f78a08c4059c20c5087cb222f6e82bfcbf9
Leonardo Iarossi <iarossi.leonardo@itismeucci.com>
2024-04-03 19:05:10 +0200

    creato panel

--- main/src/main/java/simbanca/Main.java
+++ main/src/main/java/simbanca/Main.java
@@ -1,7 +1,17 @@
 package simbanca;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Avvio sistema");
+        SchermataHome frm = new SchermataHome("SimBanca");
+        frm.setVisible(true);
+        frm.setLocationRelativeTo(null);
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frm.setLayout(new BorderLayout());
+        frm.setSize(400, 400);
     }
 }
---
+++ main/src/main/java/simbanca/SchermataHome.java
@@ -0,0 +1,15 @@
+package simbanca;
+import javax.swing.*;
+
+import java.awt.HeadlessException;
+import java.awt.event.*;
+
+public class SchermataHome extends JFrame{
+
+    public SchermataHome(String title) throws HeadlessException {
+        super(title);
+        JPanel panel = new JPanel();
+    }
+    
+    
+}
